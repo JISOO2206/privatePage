a34518fded297cbe0093efe45dd3f5e2419fa9de
JISOO2206 <900522z@gmail.com>
2022-04-26 23:58:59 +0900

    JS training day 2-2 login making start

---
+++ JS/Login.js
@@ -0,0 +1,11 @@
+const loginForm = document.querySelector(".login-form");
+const loginInput = loginForm.querySelector("input");
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    const userName = loginInput.value;
+    console.log(userName);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
